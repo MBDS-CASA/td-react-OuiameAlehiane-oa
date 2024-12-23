5f70183e459d0b708afcfec886b75dfcaf575a25
OuiameAlehiane <ouiame912@gmail.com>
2024-12-23 15:43:52 +0100

    TD01: Ajputer un composant dans la page

---
+++ TD-React/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import image from './assets/vignette_par_defaut.jpg'
+
+function Header(){
+    return(
+        <header>
+            <img src={image}/>
+            <h1>Introduction à React </h1>
+            <h2>A la découverte des premières notions de React </h2>
+        </header>
+    )
+}
+
+function App() {
+  const [count, setCount] = useState(50)
+
+  return (
+    <>
+      <div>
+          <Header/>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + MBDS +React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
